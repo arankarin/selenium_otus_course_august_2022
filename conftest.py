395c6f035898abcd561d8374a66b798fa6fcabc0
arankarin <arankar@inbox.ru>
2022-09-20 13:15:03 +0300

    simplified the paths

--- conftest.py
+++ conftest.py
@@ -7,6 +7,9 @@ def pytest_addoption(parser):
         "--browser", default="chrome", help="browser tu run tests"
     )
     parser.addoption(
+        "--drivers", default=os.path.expanduser("~/drivers"), help="browser drivers path"
+    )
+    parser.addoption(
         "--headless", action="store_true", help="browser tu run tests"
     )
 
@@ -14,7 +17,8 @@ def pytest_addoption(parser):
 @pytest.fixture
 def driver(request):
     browser_name = request.config.getoption("--browser")
-    headless = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    drivers_path = request.config.getoption("--drivers")
 
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
@@ -22,15 +26,15 @@ def driver(request):
         if headless:
             options.headless = True
         _driver = webdriver.Chrome(
-            executable_path=os.path.expanduser("~/drivers/chromedriver"),
+            executable_path=os.path.expanduser(f"{drivers_path}/chromedriver"),
             options=options
         )
     elif browser_name == "firefox":
-        _driver = webdriver.Firefox(executable_path=os.path.expanduser("~/drivers/geckodriver"))
+        _driver = webdriver.Firefox(executable_path=os.path.expanduser(f"{drivers_path}/geckodriver"))
     elif browser_name == "opera":
-        _driver = webdriver.Opera(executable_path=os.path.expanduser("~/drivers/operadriver"))
+        _driver = webdriver.Opera(executable_path=os.path.expanduser(f"{drivers_path}/operadriver"))
     elif browser_name == "yandex":
-        _driver = webdriver.Chrome(executable_path=os.path.expanduser("~/drivers/yandexdriver"))
+        _driver = webdriver.Chrome(executable_path=os.path.expanduser(f"{drivers_path}/yandexdriver"))
     else:
         raise ValueError(f"Браузер {browser_name} не поддерживается")
 
